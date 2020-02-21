3f87ecdca258b8ddf795d9cab7c82fa710981758
FanYu <FanYu@FanYu-PC>
2020-02-21 17:05:57 +0800

    1

--- mytest/src/com/github/mytest/MyTest.java
+++ mytest/src/com/github/mytest/MyTest.java
@@ -3,8 +3,8 @@ package com.github.mytest;
 public class MyTest {
 	public static void main(String[] args) {
 		System.out.println("11111");
-		System.out.println("12345");
-		System.out.println("1");
+		System.out.println("22222");
+		System.out.println("33333");
 	}
 
 }
