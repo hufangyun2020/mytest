016b2acc6f04610a539dab103019e6ada6828a76
hufangyun2020 <61307630+hufangyun2020@users.noreply.github.com>
2020-02-21 17:00:16 +0800

    Update MyTest.java

--- mytest/src/com/github/mytest/MyTest.java
+++ mytest/src/com/github/mytest/MyTest.java
@@ -4,11 +4,6 @@ public class MyTest {
 
 	public static void main(String[] args) {
 		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("4");
-		System.out.println("5");
-		System.out.println("6");
 	}
 
 }
